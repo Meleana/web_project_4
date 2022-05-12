63d54eef1dae6597609c62583e4f2012f5234bce
Anastasia Shavrova <shavrova29@gmail.com>
2022-05-13 02:10:00 +0300

    code first version of the progect to submit

--- README.md
+++ README.md
@@ -1,16 +1,34 @@
 # Project 4: Around The U.S.
+![Begining](./images/page_preview.png)
 
-### Overview
+## Overview
 
-* Figma
-* Images
+"Around the U.S." is an interactive page where users can add, remove, or like photos that focus on travel to the United States. All items display correctly on all popular screen sizes. And at this point, a form has been added to change the username and description.
 
-**Figma**
+### GitHub page's link**  
+* ["Practicum Art Gallery"](https://meleana.github.io/web_project_3/) 
 
+#### Project technologies
+* HTML 
+* CSS including Flex and Grid layouts and @mediascreen properties for several screen resolutions
+* JS
+* Figma design tools
+* Flat BEM file structure
+
+**Design files**
 * [Link to the project in Figma](https://www.figma.com/file/SurN1jaeEQIhuZEDMhmWWf/Sprint-4-Around-The-U.S.-desktop-mobile?node-id=0%3A1)
 
-**Images**
+#### Plan on improving the project
 
-The way you'll do this at work is by exporting images directly from Figma — we recommend doing that to practice more. Don't forget to optimize them [here](https://tinypng.com/), so your project loads faster. 
-
-Have fun with JavaScript!
+- Design discussion. Error correction/redesign/elimination of visual inaccuracies
+- Adding content
+- Improve photo quality depending on the device (screen size)
+- Adaptation/reduction of code
+- Connecting the ability to add, delete and edit photos. 
+- Adding validation to all forms
+- Create a mobile application
+- Remote server connection
+- Sending and receiving data from the server
+- Adding a likes counter
+- Ability to change profile avatar
+- Adding animation to forms and user actions
--- scripts/index.js
+++ scripts/index.js
@@ -2,28 +2,33 @@ const editButton = document.querySelector(".profile__edit-button");
 const closeButton = document.querySelector(".popup__close-button");
 const saveButton = document.querySelector(".popup__save-button");
 
-
-editButton.addEventListener("click", () => popUpProfileOpen("popupProfileEditor"));
+editButton.addEventListener("click", () =>
+  popUpProfileOpen("popupProfileEditor")
+);
 closeButton.addEventListener("click", () => popUp("popupProfileEditor"));
-saveButton.addEventListener("click", (e) => {popUpProfileSave("popupProfileEditor"); e.preventDefault();});
-
-
+saveButton.addEventListener("click", (e) => {
+  popUpProfileSave("popupProfileEditor");
+  e.preventDefault();
+});
 
 function popUp(Id) {
-    const element = document.getElementById(Id);
-    element.classList.toggle("popup__open");
+  const element = document.getElementById(Id);
+  element.classList.toggle("popup__open");
 }
 
 function popUpProfileOpen(Id) {
-    document.getElementById("name").value = document.querySelector(".profile__name").textContent;
-    document.getElementById("description").value = document.querySelector(".profile__description").textContent;
-    popUp(Id);
+  document.getElementById("name").value =
+    document.querySelector(".profile__name").textContent;
+  document.getElementById("description").value = document.querySelector(
+    ".profile__description"
+  ).textContent;
+  popUp(Id);
 }
-
 
 function popUpProfileSave(Id) {
-    document.querySelector(".profile__name").textContent = document.getElementById("name").value;
-    document.querySelector(".profile__description").textContent = document.getElementById("description").value;
-    popUp(Id);
+  document.querySelector(".profile__name").textContent =
+    document.getElementById("name").value;
+  document.querySelector(".profile__description").textContent =
+    document.getElementById("description").value;
+  popUp(Id);
 }
-
